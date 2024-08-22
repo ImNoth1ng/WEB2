b4d450d6880383b3d4f9f35ba5f40456cc1d7600
Noth1ng <luisbalcazar305@gmail.com>
2024-08-22 09:15:29 -0600

    clase 22 promt

---
+++ 20ago2024/app1.js
@@ -0,0 +1,13 @@
+/*let nombre = prompt("ingresa tu nombre");
+let edad = prompt("ingresa tu edad");
+let nacionalidad = prompt("ingresa tu nacionalidad");
+console.debug(nombre);
+
+console.log("Tu nombre es:" + nombre + ", tienes " + edad + " años y eres de nacionalidad " + nacionalidad)*/
+
+let numero1 = prompt("INgresa el numero 1");
+let numero2 = prompt("Ingresa el segundo numero");
+
+let numero3 = parseInt(numero1) + parseInt(numero2);
+console.log("Tu resultado es " + numero3)
+console.log(typeof numero1);
